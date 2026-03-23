f1fd3db0945c949663bc74e9c65047bfd760b220
Jcaceresc4 <jcaceresc4@miumg.edu.gt>
2026-03-22 22:35:37 -0600

    Se agregaron un new y un delete

---
+++ memoria_dinamica_2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+//punteros con asignacion de memoria dinamica;
+//new=reservar un espacio de memoria 
+//delete[]=liberar la memoria
+
+main(){
+    int fil = 0, col=0,**pm_notas;
+    cout<<"Ingrese la cantidad de  estudiantes: ";
+    cin>>fil;
+    cout<<"Ingrese la cantidad de  Notas por estudiante: ";
+    cin>>col;
+
+    pm_notas = new int *[fil];
+    for(int i=0;i<fil;i++){
+        pm_notas[i] = new int[col];
+    }
+    for(int i=0;i<fil;i++){
+        cout<<"__________Estudiante__________"<<i<<endl;
+        for(int ii=0;ii<col;ii++){
+            cout<<"Nota"<<ii<<" : ";
+            cin>>*(*(pm_notas+i)+ii);
+        }
+        cout<<"___________"<<endl;
+    }
+    cout<<"----------Mostrar Notas---------"<<endl;
+    for(int i=0;i<fil;i++){
+        for(int ii=0;ii<col;ii++){
+            cout<<*(*(pm_notas+i)+ii)<<endl;
+        }
+    cout<<"________________________________"<<endl;
+    }
+    for(int i=0;i<fil;i++){
+        delete[] pm_notas[i];
+    }
+    delete[] pm_notas;
+    system("pause");
+}
